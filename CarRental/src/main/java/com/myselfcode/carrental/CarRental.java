ac4a0fca2978ecd5b8d40fa97fefc36238035b68
Erman Louie Faminiano <119798034+ditosoft-erman@users.noreply.github.com>
2024-02-07 20:34:14 +0800

    Init repository

---
+++ CarRental/src/main/java/com/myselfcode/carrental/CarRental.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myselfcode.carrental;
+import Authentication.Login;
+import system.CustomerRegistration;
+
+/**
+ *
+ * @author Vanessa
+ */
+
+
+public class CarRental {
+
+    public static void main(String[] args) {
+        Login lf = new Login();
+       lf.show();
+       
+      
+    }
+}
